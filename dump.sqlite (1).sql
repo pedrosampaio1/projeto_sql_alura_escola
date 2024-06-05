a387e11153a1c5a957a38c780bcdf22cb6e04174
Pedro Sampaio <105110684+pedrosampaio1@users.noreply.github.com>
2024-06-05 20:28:21 -0300

    Add files via upload

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,64 @@
+-- TABLE
+CREATE TABLE Alunos (
+    ID_Aluno INT PRIMARY KEY,
+    Nome_Aluno VARCHAR(255),
+    Data_Nascimento DATE,
+    Genero VARCHAR(50),
+    Endereco VARCHAR(255),
+    Telefone_Contato VARCHAR(20),
+    Email VARCHAR(255)
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE Disciplinas (
+    ID_Disciplina INT PRIMARY KEY,
+    Nome_Disciplina VARCHAR(255),
+    Descricao TEXT,
+    Carga_Horaria INT,
+    ID_Professor INT,
+    FOREIGN KEY (ID_Professor) REFERENCES Professores(ID_Professor)
+);
+CREATE TABLE Notas (
+    ID_Nota INT PRIMARY KEY,
+    ID_Aluno INT,
+    ID_Disciplina INT,
+    Nota DECIMAL(5, 2),
+    Data_Avaliacao DATE,
+    FOREIGN KEY (ID_Aluno) REFERENCES Alunos(ID_Aluno),
+    FOREIGN KEY (ID_Disciplina) REFERENCES Disciplinas(ID_Disciplina)
+);
+CREATE TABLE Professores (
+    ID_Professor INT PRIMARY KEY,
+    Nome_Professor VARCHAR(255),
+    Data_Nascimento DATE,
+    Genero VARCHAR(50),
+    Telefone_Contato VARCHAR(20),
+    Email VARCHAR(255)
+);
+CREATE TABLE Turmas (
+    ID_Turma INT PRIMARY KEY,
+    Nome_Turma VARCHAR(255),
+    Ano_Letivo INT,
+    ID_Professor_Orientador INT,
+    FOREIGN KEY (ID_Professor_Orientador) REFERENCES Professores(ID_Professor)
+);
+CREATE TABLE Turma_Alunos (
+    ID_Turma INT,
+    ID_Aluno INT,
+    PRIMARY KEY (ID_Turma, ID_Aluno),
+    FOREIGN KEY (ID_Turma) REFERENCES Turmas(ID_Turma),
+    FOREIGN KEY (ID_Aluno) REFERENCES Alunos(ID_Aluno)
+);
+CREATE TABLE Turma_Disciplinas (
+    ID_Turma INT,
+    ID_Disciplina INT,
+    PRIMARY KEY (ID_Turma, ID_Disciplina),
+    FOREIGN KEY (ID_Turma) REFERENCES Turmas(ID_Turma),
+    FOREIGN KEY (ID_Disciplina) REFERENCES Disciplinas(ID_Disciplina)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
